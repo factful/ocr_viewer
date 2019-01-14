09736df1f96884fa4f4ea21b3ee838aa18baeaaa
Ted Han <ted@knowtheory.net>
2019-01-13 16:04:47 -0800

    Display diffs.

--- src/main.js
+++ src/main.js
@@ -1,18 +1,27 @@
 import App from './App.html';
+import Diff from './jsdiff.js';
 
-import contents from './contents.js'
-let entries = Object.entries(contents).map( (row)=>{ return row; } )
+import contents from './contents.js';
+
+let groundTruth = contents["ground-truth"];
+let image_path = contents['original'];
+let entries = Object.entries(contents).map((row)=>{ 
+	let name = row[0];
+	let original = row[1];
+	let diff = Diff.diffString(groundTruth, original);
+	return [name, original, diff]; 
+});
+entries.shift();
 
 const app = new App({
 	target: document.body,
 	data: {
-		image: entries.shift(),
+		image: image_path,
 		pages: entries,
-		activePage: entries[0][0]
-	},
-	methods: {
+		groundTruth: contents["ground-truth"],
+		activePage: entries[0][0],
+		showDiff: true
 	}
 });
-
 
 export default app;
