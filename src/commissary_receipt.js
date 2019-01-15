76b69c7efecfd4adb3e46c224545ac75e93dd9d1
Amanda on Rajje <amanda@factful.io>
2019-01-15 11:42:52 -0800

    organizing a viewer for the commissary receipt.

---
+++ src/commissary_receipt.js
@@ -0,0 +1,91 @@
+export default {
+	"Original": "/images/receipt-2esae_commissary.jpg",
+	"Transcript": `10/30/07                         10:58
+
+  THE CITY OF NEW YORK
+  DEPARTMENT OF CORRECTION
+
+  OBCC
+
+  BACA,MICHAEL
+  ID#1410720441
+
+  COMMISSARY PURCHASE
+  REFERENCE NUMBER:1187090735
+
+  ITEM                  PRICE  #   TOTAL
+  ORANGCE SLICE       $ 1.40  1 $   1.40
+  CHEEZ-IT            $ 2.72  1 $   2.72
+  TOWN HOUSE CRACKERS $ 3.47  1 $   3.47
+  CHEWY CHIPS AHOY    $ 3.40  1 $   3.04
+  RAMEN SOUP          $ 0.35 15 $   5.25
+  BEEFSTICK           $ 0.31  6 $   1.86
+  NACHO CHEESE DORITO $ 0.45  3 $   1.35
+  KOOL AID GRAPE 6OZ. $ 1.38  1 $   1.38
+  CLEAR TANG          $ 1.47  1 $   1.47
+  SLOPPY JOE          $ 1.90  2 $   3.80
+  KRAFT MAC & CHEESE  $ 0.98  2 $   1.96
+  HONEY BUN           $ 0.65  5 $   3.25
+  HUDSON COFFEE       $ 0.59  4 $   2.36
+  TUNA                $ 1.53  2 $   3.06
+  PEANUT BUTTER       $ 2.03  2 $   4.06
+  FRESH SCENT         $ 0.76  2 $   1.52
+  SNICKERS            $ 0.66  4 $   2.64
+  $.41STAMPED ENVELOP $ 0.49  7 $   3.43
+  SHOWER SLIPPERS     $ 0.72  1 $   0.72
+  WRITING TABLET      $ 1.00  1 $   1.00
+  PEN                 $ 0.42  1 $   0.42
+  TOTAL               ------ -- $  50.16
+
+  THANK YOU`,
+  "OCRopus": ``,
+	"Tesseract": `10/30/07
+
+  k CAPS Nol Keeby ae a
+  \ 1; 10:58
+
+  THE CITY OF NEW me
+  DEPARTMENT OF =
+
+  OBCC
+
+  BACA , MICHAEL
+  ID#1410720441
+
+  COMMISSARY PURCHASE
+
+
+
+  REFERENCE NUMBER: 1187090735
+
+  ITEM PRICE # TOTAL
+  ORANGE SLICE $1.40 16 9) ae
+  CHEEZ-IT $2772 15 27
+  TOWN HOUSE CRACKERS $. 3.47...1 S... 3.4]
+  k CHEWY CHIPS AHOY eee |
+  RAMEN SOUP $6.55 15 4 5.254
+  BEEFSTICK ¢ 0.31 6 € 256
+  NACHO CHEESE DORITO $ 0.45 3. 1.35
+  WOOL AID GRAPE 662. 6 1.38 2-@.4.38
+  CLEAR TANG $4547 4 82 tay
+  SLOPPY JOE @4,00 26 4.40
+  KRAFT MAC & CHEESE $ 0.98 25 1.96
+  HONEY BUN €0.65 66 3.25%
+  HUDSON COFFEE $90.59 4 6 ~2eae
+  TUNA @ 163 26. 2.06
+  PEANUT BUTTER 6.9.8: 2%. 4 Ge
+  FRESH SCENT S 0.76 2 6 21 82
+  SNICKERS oe 66 46° 2.64
+  $.41STAMPED ENVELOP $ 0.49 7$ 3.43
+  SHOWER SLIPPERS BO 72 1s. 872
+  WRITING TABLET & 4.00 1 S 1280
+  PEN f@45 1 6. 6.42
+  ct -.08 80.16
+
+  ___THANK YOU CoME AGAIN cao
+  `,
+  "Adobe": ``,
+	"Abbyy": ``,
+  "Google": ``,
+	"Azure": ``
+}
