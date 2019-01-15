943fde6a1909658067d76c28c2330685335bee7b
Amanda on Rajje <amanda@factful.io>
2019-01-15 12:01:37 -0800

    added all existing texts

--- src/commissary_receipt.js
+++ src/commissary_receipt.js
@@ -83,9 +83,327 @@ export default {
   ct -.08 80.16
 
   ___THANK YOU CoME AGAIN cao
-  `,
-  "Adobe": ``,
-	"Abbyy": ``,
-  "Google": ``,
-	"Azure": ``
+`,
+  "Adobe": `BACA,MICHAEL
+  ID#1410720441
+  COMMISSARY PURCHASE
+  REFERENCE NUMBER:1187090735
+  ITEM
+  PRICE
+  ORANGE SLICE
+  $ 1.40
+  CHEEZ-IT
+  $ 2.72
+  TOWN
+  _ HOUSE CRACKERS _ 3.47
+  CHEWY CHIPS AHOY
+  $ 3.04
+  RAMEN SOUP
+  $ 0.35
+  BEEFSTICK
+  $ 0.31
+  NACHO CHEESE DORITO $ 0.45
+  KOOL AID GRAPE 6OZ. $ 1. 38
+  CLEAR TANG
+  $ 1.47
+  SLOPPY JOE
+  $ 1. 90
+  KRAFT MAC & CHEESE $ 0.98
+  HONEY BUN
+  $ 0.65
+  HUDSON COFFEE
+  $ 0.59
+  TUNA
+  $ 1. 53
+  PEANUT BUTTER
+  $ 2.03
+  FRESH SCENT
+  $ 0.76
+  SNICKERS
+  $ 0.66
+  $.41STAMPED ENVELOP $ 0.49
+  SHOWER SLIPPERS
+  $ 0.72
+  WRITING TABLET
+  $ 1.00
+  PEN
+  $ 0.42
+  -----TOTAL
+
+  .*
+
+  THANK Yoq
+
+  C,oMEAe,n\N
+  ,,,,
+  "
+
+  #
+  1
+  1
+  _1
+  1
+  15
+  6
+  3
+  1
+  1
+  2
+  2
+  5
+  4
+  2
+  2
+  2
+  4
+  7
+  1
+  1
+  1
+
+  $
+  $
+  $
+  $
+  $
+  $
+  $
+  $
+  $
+  $
+  $
+  $
+  $
+  $
+  $
+  $
+  $
+  $
+  $
+  $
+  $
+
+  TOTAL
+  1.40
+  2.72 ,
+  . __L. 42__
+  3.04
+  5.25 ~
+  1. 86
+  1. 35
+  1.38
+  1.47
+  3.80
+  1. 96
+  3.25 '
+  2.36
+  3.06
+  4.06
+  1. 52
+  2.64
+  3.43
+  0.72
+  1.00
+  0.42
+  50.16
+
+`,
+	"Abbyy": `﻿k CVaYS NnuKee^
+
+                    10/30/07    ^ ------UT--00
+                                                                  10 : 58
+
+                    THE CITY OF NEW YORK
+                    DEPARTMENT OF CORRECTION
+                                                v vm
+   OBCC
+
+   BACA,MICHAEL
+
+   ID#1410720441
+                                                                     / "
+   COMMISSARY PURCHASE
+                                                   W          \
+   REFERENCE NUMBER:1187090735                                        <9
+
+  ITEM                                          PRICE     #        TOTAL
+  ORANGE SLICE    $                             1.40      1  $      1.40
+  CHEEZ-IT    $                                 2.72      1  $      2.72
+  TOWN HOUSE CRACKERS $                         3.47      1  _$     3.47
+  X CHEWY CHIPS AHOY $                          3.04      1  $      3.04
+  RAMEN SOUP    $                                         .5 $      5.25
+  BEEFSTICK    $                                0.31      6  $      1.86
+  NACHO CHEESE DORITO $                         0.45      3  $      1.35
+  KOOL AID GRAPE 60Z. $                         1.38      1  $      1.38
+  CLEAR TANG    $                               1.47      1  $    1       47
+  SLOPPY JOE    $                               1.90      2  $    3       80
+  KRAFT MAC & CHEESE $                          0.98      2  $   1        96
+  HONEY BUN    $                                0.65      5  $   3        25v
+  HUDSON COFFEE    $                            0.59      4  $   2        36
+  TUNA    $                                     1.53      2  $   3        06
+  PEANUT BUTTER    $                            2.03      2  $   4        06
+  FRESH SCENT    $                              0.76      2  $   1        52
+  SNICKERS    $                                 0.66      4  $   2        64
+  $.41STAMPED ENVELOP $                         0.49      7  $   3        43
+  SHOWER SLIPPERS    $                          0.72      1  $      0.72
+  WRITING TABLET    $                           1.00      1  $      1.00
+  PEN    $                                      0.42      1  $      0.42
+  TOTAL                                                      $     50.16
+                           C-qme. Again,
+   THANK YOU
+`,
+  "Google": `10/30/07
+  10:58
+  THE CITY OF NEW YORK
+  DEPARTMENT OF CORRECTION
+  OBCC
+  BACA, MICHAEL
+  ID#1410720441
+  COMMISSARY PURCHASE
+  REFERENCE NUMBER: 1187090735
+  PRICE # TOTAL
+  1.40 1$ 1.40
+  $ 2.72 1 2.72
+  ITEM
+  ORANGE SLICE
+  CHEEZ-IT
+  TOWN HOUSE CRACKERS $ 3.47 1$3.47
+  CHEWY CHIPS AHOY 3.04 1 3.04
+  RAMEN SOUP
+  BEEFSTICK
+  NACHO CHEESE DORITO $ 0.45 3 $ 1.35
+  KOOL AID GRAPE 60Z. $ 1.38 1 $ 1.38
+  CLEAR TANG
+  SLOPPY JOE
+  KRAFT MAC & CHEESE 0.98 2$ 1.96
+  HONEY BUN
+  HUDSON COFFEE
+  TUNA
+  PEANUT BUTTER
+  FRESH SCENT
+  SNICKERS
+  $. 41STAMPED ENVELOP $ 0.49 7$ 3.43
+  SHOWER SLIPPERS
+  WRITING TABLET
+  PEN
+  TOTAL
+  $ 0.35 15 $ 5.25
+  0.31 6 $ 1.86
+  1.47 1 $1.47
+  $1.90 2 $ 3.80
+  0.65 5 3.25
+  0.59 4 $ 2.36
+  $ 1.53 2 $ 3.06
+  $ 2.03 2$4.06
+  S 0.76 2 $ 1.52
+  S 0.66 4 $2.64
+  $ 0.72 1 $ 0.72
+  $ 1.00 1$ 1.00
+  0.42 1 0.42
+  -- $ 50.16
+  THANKYOU COME ACAİN"",,
+  THANK YOU
+  IJN ルAGAINlⅢ//
+`,
+	"Azure": `10/30/07
+  THE CİTY OF NEW YORK
+  DEPARTMENT OF CORRECTION
+  OBCC
+  BACA , Ml CHAM
+  ID#1410720441
+  COMMISSARY PURCHASE
+  REFERENCE NUMBER•. 1187090735
+  10:58
+  TOTAL
+  İTEM
+  ORANGE SLICE
+  CHEEZ- IT
+  TOWN HOUSE CRACKERS
+  CHEWY CHİPS AHOY
+  RAMEN SOUP
+  BEEFSTICK
+  NACHO CHEESE DORITO
+  KOOL AID GRAPE .
+  CLEAR TANG
+  SLOPPY JOE
+  KRAFT MAC & CHEESE
+  HONEY BUN
+  HUDSON COFFEE
+  TUNA
+  PEANUT BUTTER
+  FRESH SCENT
+  SNICKERS
+  $ . 4 ISTAMPED ENVELOP
+  SHOWER SLIPPERS
+  WRITING TABLET
+  PEN
+  TOTAL
+  PRICE
+  1.40
+  2.72
+  2.72
+  3.47
+  3.47
+  3.04
+  5.25
+  0.35
+  1.86
+  0.31
+  0.45
+  1.38
+  1.47
+  3.80
+  1.90
+  0.98
+  2.36
+  1.53
+  4.06
+  2.03
+  50.16
+  40
+  04
+  65
+  o.
+  59
+  0.76
+  66
+  o.
+  o.
+  0.72
+  1.00
+  42
+  1
+  1
+  1
+  1
+  15
+  6
+  3
+  1
+  1
+  2
+  2
+  5
+  4
+  2
+  2
+  2
+  4
+  7
+  1
+  1
+  1
+  1.35
+  1.38
+  1.47
+  1.96
+  3.25 N
+  3.06
+  1.52
+  3.43
+  0.72
+  1.00
+  YOU
+  COME
+`
 }
