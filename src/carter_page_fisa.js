7109371d6f1947028ab5df4b82c85d6a9a04cc80
Amanda on Rajje <amanda@factful.io>
2019-01-15 14:52:29 -0800

    Adding carter page FISA

---
+++ src/carter_page_fisa.js
@@ -0,0 +1,226 @@
+export default {
+	"Original": "/images/legal-carter_page_fisa.jpg",
+	"Transcript": ``,
+//  "OCRopus": ``,
+	"Tesseract": `All information is considered
+	unclassified except where FOPSECRETHNOFORN:
+
+	otherwise shown,
+
+
+
+	UNITED STATES
+	Classify ese FOREIGN INTELLIGENCE SURVEILLANCE COURT
+	eason:
+	‘ bi-1
+	Derived From: FBINSICG, dated 10-2016
+	Declassfy On: WASHINGTON, D.C. et
+	(® IN RE CARTER W. PAGE, A U.S. Docket Number:
+
+	PERSON.
+
+
+
+
+
+	(®) The United States of America hereby applies to this Court for authority to
+
+	cond ic is described herein, pursuant bi-1
+
+	b3-1
+	to the Foreign Intelligence Surveillance Act of 1978, as amended, Title 50, United ven 236
+	States Code (U.S.C.), $5 ENN (FISA or the Act).
+	1. (U) Identity of Federal Officer Making Application This application is
+	made by EMM a Supervisory Special Agent (SSA) of the Federal bet
+	3-1
+	Bureau of Investigation (FBI) whose official duties at FBI Headquarters include na
+	supervision of the FBI's investigation of the above-captioned target based upon ae 36
+
+	information officially furnished to JE
+
+	FOR SECRETYNOFORN:
+
+	Classified by: te
+	Derived from: A brat
+	Declassify on:
+
+	OI Tracking No. 143045
+`,
+  "Adobe": `All information is considered
+	unclassified except where
+	otherwise shown .
+
+	TOP SECR:ET/,'NOl'OR."I
+	UNITED STATES
+
+	Classify by: )23)98T32
+
+	FOREIGN INTELLIGENCE SURVEILLANCE COURT
+
+	Reason: (C)
+	Deriv ed From : FBI N SICG , dated 10.
+	Declassify On:-
+
+	-2016
+
+	bl-1
+	b3-l
+	b7A-1
+
+	WASHINGTON, D.C.
+
+	(S) IN RB CARTER W. PAGE,A U.S.
+
+	Docket Number:
+
+	PERSO N .
+
+	(0) VERIF
+	IED APPLICATION
+
+	(X) The United States of America hereby applies to this Court for authority to
+	conduct
+
+	, as descri bed herein, pursuant
+
+	bl - I
+	b3-I
+	b7A- I
+	b7E-1, 2, 3, 6
+
+	to the Foreign Intelligence Surveillance Act of 1978, as amended , Title 50, United
+	States Code (U.S.C .), §§
+	1.
+
+	(PISA or the Act).
+
+	(U) Identity of Federal Officer
+
+	inadeby
+
+	Ma.king
+	App lication
+
+	This application is
+
+	, a Supervisory Special Agent (5.5A) of the Federal
+
+	Bureau of Investigati on (FBI) whose official duties at FBI Headquarters include
+	supervision of the FBI's investigation of the above-captioned target based upon
+
+	bl - I
+	b3-l
+	b6-l
+	b7A-l
+	b7C- l
+	b7E- 1, 2, 3, 6
+
+	information officially furnished to
+
+	TOP SECR:131'//NOl'OR."I
+	Class ified by:
+
+	bl-I
+	b3-I
+
+	Deri ved from:
+
+	b7A-1
+
+	Declassify on:
+
+	-
+
+	01 Tracking No. 143045
+
+`,
+//	"Abbyy": ``,
+  "Google": `All information is considered
+	unclassified except where
+	otherwise shown.
+	Classify by: J23J98T32
+	Reason: (C)
+	Derived From: FBI NSICG, dated 10
+	Declassify On
+	UNITED STATES
+	FOREIGN INTELLIGENCE SURVEILLANCE COURT
+	WASHINGTON, D.C.
+	b1-1
+	b3-1
+	b7A-1
+	-2016
+	(8) INRE CARTER W. PAGE, A U.S
+	Docket Number:
+	PERSON
+	(S) The United States of America hereby applies to this Court for authority to
+	conduct
+	to the Foreign Intelligence Surveillance Act of 1978, as amended, Title 50, United
+	States Code (US.С.), SS
+	as described herein, pursuant bl-1
+	b3-1
+	b7A-1
+	b7E-1,2, 3, 6
+	(FISA or the Act)
+	dentit
+	made
+	a Supervisory Special Agent (SSA) of the Federal
+	Bureau of Investigation (FBI) whose official duties at FBI Headquarters include
+	supervision of the FBI's investigation of the above-captioned target based upon
+	information officially furnished to
+	b1-1
+	b3-1
+	b6-1
+	b7A-1
+	b7C-1
+	b7E-1,2,3,6
+	Classified by:
+	Derived from:
+	Declassify on:
+	b1-1
+	b3-1
+	b7A-1
+	OI Tracking No. 143045`,
+	"Azure": `All information is considered
+	unclassified except where
+	otherwise shown.
+	Classify by. J23J98T32
+	Reason: (C)
+	UNITED STATES
+	FOREIGN INTELLIGENCE SURVEILLANCE COURT
+	Derived From: FBINSICG, dated 10'-2016
+	WASHINGrON, D.C.
+	Declassi$r On:
+	(S IN RE CARTER W. PAGE,A U.S.
+	PERSON.
+	bi-l
+	b3-1
+	b7A-1
+	Docket Number:
+	(S The United States Of America hereby applies to this Court for authority to
+	conduct
+	, as described herein, pursuant bl-l
+	b3-1
+	to the Foreign Intel Surveillan Act Of 1978, as amende d, Ti fle 50, United b7A-l
+	States Code (U SC.), SS
+	b7E-l, 2.3, 6
+	(FISA or the Act).
+	1. (U) Identity of Federal Officer Making This application is
+	made by
+	, a Supervisory Special Agent (SSA) Of the Federal
+	Bureau Of Investigation (FBI) whose official duties at FBI Headquarters include
+	supervision of the FBI's investigation Of the above-captioned target based upon
+	information officially furnished to
+	Classified by:
+	Derived from:
+	Declassify on:
+	bl.l
+	b3-1
+	b6-l
+	b7A-1
+	b7c-1
+	bi-l
+	b3-l
+	b7A-1
+	01 143045
+`
+}
