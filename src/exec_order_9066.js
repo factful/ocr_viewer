977bee752354bca4621e7a44a7ebfe557d6ff7f7
Amanda on Rajje <amanda@factful.io>
2019-01-14 16:15:11 -0800

    Order tabs to match post.

--- src/exec_order_9066.js
+++ src/exec_order_9066.js
@@ -1,6 +1,6 @@
 export default {
 	"Original": "/images/historical-executive_order_9066-japanese_internment.jpg",
-  	"Transcript": `
+	"Transcript": `
 EXECUTIVE ORDER
 
 AUTHORIZING THE SECRETARY OF WAR TO PRESCRIBE
@@ -27,6 +27,31 @@ from which any or all persons may be excluded, and with
 respect to which, the right of any person to enter, re-
 main in, or leave shall be subject to whatever restric-
 tions the Secretary of War or the appropriate Military
+`,
+"OCRopus": `
+	nationel8efenee premisse, en6 netional-8efense util-
+	main in, or lesve ahall be subjeot to hetever reatric-
+	tione the Seoretary of Wer or the sporopriete H 11itary
+	aUnoIedmo THE ssoEEaEY oF wAR To FRSORIBE
+	?espeet to vhieh, the rigt of any person to enter, re-
+	vested in me ee Presi8ent of the Dnited States, an
+	MO, 7SRPOP, by virtue or the authority
+	frcs which any or all peirsons mey be exeluded, and Yith
+	SSEOUrIYE oEDrR
+	militskry areas in suob peces an6 of sueh ertent as he
+	Comen8er in Chief of the Arsy sn8 evy, I hereby
+	-
+	or tbe agpropriete .ilitery Commandeir Aaay 8eteraine,
+	55 stet. 655 (7. s. ., Tit1e 50, smo. 304)
+	IIi2itary Commanders whom he aaey from tiae to time
+	H5REAS the auceeseful proeeeution of the war
+	1TdTaEY ARSaS
+	autborige an8 3ireet tbe Secretary of Wer, en6 the
+	190, 54 stet, 120, ene the Aet or Aoguet ?1, 1943,
+	?eguires every poesible proteetion against e8piOnege
+	deema suoh aotion neeessaxry or 8esireble, %0 9reaeribe
+	and against sebotege to national-efenee aeteriel,
+	40 8tat. 533, as amen6e6 by the Aet or November 30,
 `,
 	"Tesseract": `‘=XECULIVE ORDER
 
@@ -56,87 +81,11 @@ main in, or leave shall be subject to whatever restric-
 
 itary
 
- 
+
 
 tions the Secretary of War or the eppropriate
 `,
-  "Google": `
-EXECUTIVE ORDRR
-AUTHORIZING THE SECRETARY OF WAR TO PRESCRIBE
-ILITARY AREAS
-WEEREAS the successful prosecution of the wer
-requires every possible protection ageinst espionage
-and against sabotage to national-defense materiel,
-nationel-defense premises, and national-defense util-
-ities as defined in Section 4, Act of April 20, 1918,
-40 Stat. 533, es amended by the Act of November 30,
-1940, 54 Stat. 1220, end the Act of August 21, 1941,
-55 stat. 655 (U. S. С., Title 50, Sec. 104):
-NOM, THEREby virtue of the authority
-vested in me as President of the United States, end
-Commender in Chief of the Army and Navy, I hereby
-authorize and direct the Secretary of War, and the
-Military Commenders whom he mey from time to time
-designate, whenever he or any designated Commander
-deems such action necessary or desirable, to prescribe
-military areas in such places and of such extent as he
-or the appropriate ilitary Commander may determine,
-from which any or all persons may be excluded, and with
-respect to which, the right of any person to enter, re-
-main in, or leeve shall be subject to whatever restric-
-tions the Secretary of War or the eppropriate hilitery
-`,
-	"Azure": `ORDER
-SBCR31'ÆkY OF WAR PESCRTBE
-AREAS
-i•ÆEREAS the successful prosecution of the war
-requires every possible protection against espionage
-and against sabotage to national—defense material,
-national—defense premises, and national-defense utll—
-ities es defined in Section Act of April 191B,
-40 Stat. 533, es amended by the Act of November 30,
-1940, 54 Stat. 1220, end the Act of August 21, 1941,
-55 stet. 655 (V. S. C. , Title 50, see. 104):
-wt.', by virtue of the authority
-vested in me as President of the United States, end
-Cormanaer in Chief of the Army and Navy, I hereby
-authorize and direct the Secretary Of end the
-Military Cornmendors whom he may from time to time
-designate, whenever he or any åeeigaeted Commander
-deems such action necessary or desirable, to prescribe
-military areas in such pieces and of gueh extent es he
-or the appropriate Xilitary Conmander may aetemlne,
-from which any or ell persons may be excluded, and With
-respect to which, the of any person to enter, re—
-mein Ir., Or leave shall be subject to "hetever reetrlc—
-ticng the Secretary of War or the eppropri@te Ell itary
-`,
-  "OCRopus": `
-	nationel8efenee premisse, en6 netional-8efense util-
-	main in, or lesve ahall be subjeot to hetever reatric-
-	tione the Seoretary of Wer or the sporopriete H 11itary
-	aUnoIedmo THE ssoEEaEY oF wAR To FRSORIBE
-	?espeet to vhieh, the rigt of any person to enter, re-
-	vested in me ee Presi8ent of the Dnited States, an
-	MO, 7SRPOP, by virtue or the authority
-	frcs which any or all peirsons mey be exeluded, and Yith
-	SSEOUrIYE oEDrR
-	militskry areas in suob peces an6 of sueh ertent as he
-	Comen8er in Chief of the Arsy sn8 evy, I hereby
-	-
-	or tbe agpropriete .ilitery Commandeir Aaay 8eteraine,
-	55 stet. 655 (7. s. ., Tit1e 50, smo. 304)
-	IIi2itary Commanders whom he aaey from tiae to time
-	H5REAS the auceeseful proeeeution of the war
-	1TdTaEY ARSaS
-	autborige an8 3ireet tbe Secretary of Wer, en6 the
-	190, 54 stet, 120, ene the Aet or Aoguet ?1, 1943,
-	?eguires every poesible proteetion against e8piOnege
-	deema suoh aotion neeessaxry or 8esireble, %0 9reaeribe
-	and against sebotege to national-efenee aeteriel,
-	40 8tat. 533, as amen6e6 by the Aet or November 30,
-`,
-	"Adobe": `
+"Adobe": `
 \l'd!RltAStheeueeee&f'ulproMeutionotthewar
 req,uiree
 
@@ -235,30 +184,81 @@ or the 11ppl'¢l>ri11teHl1tary
 -
 `,
 	"Abbyy": `
- EXECUTIVE                                               
-                                                          
-AUTHORIZING THE SECRETARY OF WAR TO PRESCRIBE             
-MILITARY AREAS                                            
-WHEREAS the suooessful prosecution of the vrar            
-requires every possible protection against espionage      
-and against sabotage to national-defense material,        
-national-defense premises, and national-defense util¬     
-ities as defined in Section A, Aot of April 20, 1918,     
-40 Stat. 533, as amended by the Aot of November 30,       
-1940, 54 Stat. 1220, end the Act of August 21, 1941,      
-55 Stat. 655 (U. S. 0., Title 50, Sso. 104):              
-NOW, THEREFORE, by virtue of the authority                
-vested in me as President of the United States, and       
-Commander in Chief of the Army and Navy, I hereby         
-authorize and direct the Secretary of War, end the        
-Hilltary Coamendors whom he may from time to time         
-designate, whenever he or any designated Commander        
-deems such action necessary or desirable, to prescribe    
-military areas in such places and of such extent as he    
-or the appropriate Ililitary Commander may determine,     
-from which any or all persons may be excluded, and with   
-respect to which, the right of any person to enter, re¬   
-main in, or leave shall be subject to whatever restric¬   
-tions the Secretary of War or the appropriate Military    
+ EXECUTIVE
+
+AUTHORIZING THE SECRETARY OF WAR TO PRESCRIBE
+MILITARY AREAS
+WHEREAS the suooessful prosecution of the vrar
+requires every possible protection against espionage
+and against sabotage to national-defense material,
+national-defense premises, and national-defense util¬
+ities as defined in Section A, Aot of April 20, 1918,
+40 Stat. 533, as amended by the Aot of November 30,
+1940, 54 Stat. 1220, end the Act of August 21, 1941,
+55 Stat. 655 (U. S. 0., Title 50, Sso. 104):
+NOW, THEREFORE, by virtue of the authority
+vested in me as President of the United States, and
+Commander in Chief of the Army and Navy, I hereby
+authorize and direct the Secretary of War, end the
+Hilltary Coamendors whom he may from time to time
+designate, whenever he or any designated Commander
+deems such action necessary or desirable, to prescribe
+military areas in such places and of such extent as he
+or the appropriate Ililitary Commander may determine,
+from which any or all persons may be excluded, and with
+respect to which, the right of any person to enter, re¬
+main in, or leave shall be subject to whatever restric¬
+tions the Secretary of War or the appropriate Military
+`,
+  "Google": `
+EXECUTIVE ORDRR
+AUTHORIZING THE SECRETARY OF WAR TO PRESCRIBE
+ILITARY AREAS
+WEEREAS the successful prosecution of the wer
+requires every possible protection ageinst espionage
+and against sabotage to national-defense materiel,
+nationel-defense premises, and national-defense util-
+ities as defined in Section 4, Act of April 20, 1918,
+40 Stat. 533, es amended by the Act of November 30,
+1940, 54 Stat. 1220, end the Act of August 21, 1941,
+55 stat. 655 (U. S. С., Title 50, Sec. 104):
+NOM, THEREby virtue of the authority
+vested in me as President of the United States, end
+Commender in Chief of the Army and Navy, I hereby
+authorize and direct the Secretary of War, and the
+Military Commenders whom he mey from time to time
+designate, whenever he or any designated Commander
+deems such action necessary or desirable, to prescribe
+military areas in such places and of such extent as he
+or the appropriate ilitary Commander may determine,
+from which any or all persons may be excluded, and with
+respect to which, the right of any person to enter, re-
+main in, or leeve shall be subject to whatever restric-
+tions the Secretary of War or the eppropriate hilitery
+`,
+	"Azure": `ORDER
+SBCR31'ÆkY OF WAR PESCRTBE
+AREAS
+i•ÆEREAS the successful prosecution of the war
+requires every possible protection against espionage
+and against sabotage to national—defense material,
+national—defense premises, and national-defense utll—
+ities es defined in Section Act of April 191B,
+40 Stat. 533, es amended by the Act of November 30,
+1940, 54 Stat. 1220, end the Act of August 21, 1941,
+55 stet. 655 (V. S. C. , Title 50, see. 104):
+wt.', by virtue of the authority
+vested in me as President of the United States, end
+Cormanaer in Chief of the Army and Navy, I hereby
+authorize and direct the Secretary Of end the
+Military Cornmendors whom he may from time to time
+designate, whenever he or any åeeigaeted Commander
+deems such action necessary or desirable, to prescribe
+military areas in such pieces and of gueh extent es he
+or the appropriate Xilitary Conmander may aetemlne,
+from which any or ell persons may be excluded, and With
+respect to which, the of any person to enter, re—
+mein Ir., Or leave shall be subject to "hetever reetrlc—
+ticng the Secretary of War or the eppropri@te Ell itary
 `
 }
