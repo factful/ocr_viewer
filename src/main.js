0b03affd5592451a329ffb425b5bd1d3a54d20c5
Ted Han <ted@knowtheory.net>
2019-01-17 14:38:55 -0800

    Load all of the data

--- src/main.js
+++ src/main.js
@@ -1,14 +1,12 @@
 import App from './App.html';
 import Diff from './jsdiff.js';
 
-// import contents from './exec_order_9066.js';
-//import contents from './commissary_receipt.js';
-import contents from './carter_page_fisa.js';
-
 let prepareData = (contents) => {
+	console.log(contents["Original"]);
 	let groundTruth = contents["Transcript"];
 	let entries = Object.entries(contents).map((row)=>{
 		let name = row[0];
+		console.log(name);
 		let original = row[1];
 		let diff = Diff.diffString(groundTruth, original);
 		return [name, original, diff];
@@ -19,28 +17,27 @@ let prepareData = (contents) => {
 		groundTruth: groundTruth,
 		imagePath: contents['Original'],
 		pages: entries
-	}
+	};
 };
 
-let groundTruth = contents["Transcript"];
-let image_path  = contents['Original'];
-let entries = Object.entries(contents).map((row)=>{
-	let name = row[0];
-	let original = row[1];
-	let diff = Diff.diffString(groundTruth, original);
-	return [name, original, diff];
-});
-entries.shift();
+import execOrder9066 from './exec_order_9066.js';
+import commissaryReceipt from './commissary_receipt.js';
+import carterPageFisa from './carter_page_fisa.js';
 
+let data = {
+	execOrder9066:		 prepareData(execOrder9066),
+	commissaryReceipt: prepareData(commissaryReceipt),
+	carterPageFisa: 	 prepareData(carterPageFisa)
+};
+
+console.log(data);
 const app = new App({
 	target: document.body,
 	data: {
-		image: image_path,
-		pages: entries,
-		groundTruth: groundTruth,
-		activePage: entries[0][0],
-		showDiff: true,
-		url: entries[0][0]
+		data: data,
+		topic: Object.keys(data)[0],
+		activePage: "Transcript",
+		showDiff: true
 	}
 });
 
