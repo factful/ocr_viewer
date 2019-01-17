0f28a47248c2becff5adf44b286f26a342d7fe09
Ted Han <ted@knowtheory.net>
2019-01-17 08:14:23 -0800

    Add a function for preparing data.

--- src/main.js
+++ src/main.js
@@ -5,6 +5,23 @@ import Diff from './jsdiff.js';
 //import contents from './commissary_receipt.js';
 import contents from './carter_page_fisa.js';
 
+let prepareData = (contents) => {
+	let groundTruth = contents["Transcript"];
+	let entries = Object.entries(contents).map((row)=>{
+		let name = row[0];
+		let original = row[1];
+		let diff = Diff.diffString(groundTruth, original);
+		return [name, original, diff];
+	});
+	entries.shift();
+
+	return {
+		groundTruth: groundTruth,
+		imagePath: contents['Original'],
+		pages: entries
+	}
+};
+
 let groundTruth = contents["Transcript"];
 let image_path  = contents['Original'];
 let entries = Object.entries(contents).map((row)=>{
@@ -22,7 +39,8 @@ const app = new App({
 		pages: entries,
 		groundTruth: groundTruth,
 		activePage: entries[0][0],
-		showDiff: true
+		showDiff: true,
+		url: entries[0][0]
 	}
 });
 
