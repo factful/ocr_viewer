a3003407038457eba9aa0b8544c4512cb895f9fb
Amanda on Rajje <amanda@factful.io>
2019-01-16 16:33:02 -0800

    Still need to pick a starting point for the transcript

---
+++ src/texas_campaign_filing.js
@@ -0,0 +1,548 @@
+export default {
+	"Original": "images/form-tx_campaign_filing.png",
+	"Transcript": ``,
+  "OCRopus": `
+	other SVR gent- Podobnvy and Sporvshev to gather mtellgence un behalf ot
+	employee tn the Mardy4ttYt otfuue of a Russan bank buryakoy worked wth two
+	and gor Spmvshev were charged by a scaled complant m the U 5 D1Mrruct Court
+	b/E 1 2
+	b'G 2
+	(') 3g63) I or about Jauary 201- Podobnyy alugg wh Evgeny Buryako
+	E*E 1 2 6
+	b1 1
+	wmm\ 0W. FBI
+	b'A 1
+	HR >
+	uonspug to ut mnd autng a n unregi-turcd aeent ot a focign governmenb)
+	tor the Southern Atrict of New York te va.labons of 1S U S C 6y 3'T and 951
+	h3 1
+	be 2
+	Acuordmg to the complant Bury kov worked um he Urrted States as an aget ut the
+	h74 1
+	b7C :
+	SYR pec:tcally Burvakov uperated under non otfucal cover posuung as an
+	1
+`,
+	"Tesseract": `
+
+	MONETARY POLITICAL CONTRIBUTIONS.
+
+	SCHEDULE A1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	i i ' 1 Total pages Schedule At
+	The Instruction Guide explains how to complete this form. Ssh: 23 Rpt $77
+	FILER NAME [3 Fler1D (Ethos Commission Filers)
+	Citizens for a Quality Judiciary 00082952
+	Date 5 Fullname of contibutor  [] ouvotstate PAG (0% [7 Amount of Contribution (3)
+	08/07/2018 | Larry M Hicks CPA $100.00
+	5” Convibuior address: City: State: Zip Code
+	Houston, TX 77092
+	Principal occupation Job ile (See instructions) [2 Employer (See instructions)
+	CPA seit
+	Date Fullname of contibutor LJ eutotsiate PAC (0 "Amount of Contribution (8)
+	09/10/2018 | Law Office of Mitchell R Nelson PLLC $2,500.00
+	Conitibuior address, City: Sie: Zip Code
+	Houston, TX 77008
+	Principal occupation Job ile (See instructions) Employer (See instructions)
+	Date Fullname of contibutor LJ eutotsiate PAC (0 "Amount of Contribution (8)
+	09/27/2018 | Law Offices of Matthew Dexter PLLC $250.00
+	Conitibuior address, City: Sie: Zip Code
+	Houston, TX 77008
+	Principal occupation / Job ile (See Instuctions) Employer (See instructions)
+	Date Fullname of contibutor LJ eutotsiate PAC (0 “Amount of Contribution (8)
+	09/17/2018 | McAlister, Sean $2,500.00
+	Conitibuior address, City: Sie: Zip Code
+	Spring, TX 77379
+	Principal occupation Job ile (See Instuctions) Employer (See instructions)
+	Attorney seit
+	Date Fullname of contibutor LJ eutotsiate PAC (0 “Amount of Contribution (8)
+	09/06/2018 | McBride, Ralph $1,000.00
+
+	Contibutor address; City: State Zip Code
+
+	Houston, TX 77057
+
+
+
+
+
+
+
+	Principal occu}
+	Attorney
+
+	pation / Job tile (See Instructions)
+
+
+
+	Employer (See instructions)
+	Bracewell
+
+
+
+
+
+
+
+	“Brine provided
+
+	Dy Texas Ethics Commission
+
+	Wraw SINGS STATE DUS
+
+	Version VIBE
+
+
+`,
+  "Adobe": `MONETAR Y POLITI CAL CONTRIBUTION S
+
+	SCHEDUL .E
+
+	1 Total pages Schedu le Al :
+
+	The Instruction Guide explains how to complete this form .
+
+	Sch: 213Rpt: 517
+
+	2 FILER NAME
+
+	3 Filer 10 (Ethics Commission Filers)
+
+	Citizens for a Quality Judiciary
+	4
+
+	Date
+
+	08/0712018
+
+	Al
+
+	s
+
+	Full name o f con tributor
+
+	000829 52
+
+	0
+
+	\
+
+	out-of-slate PAC (10 #:
+
+	7 Amount o f Contribution ($)
+
+	Larry M Hicks CPA
+
+	$100 .00
+
+	.. ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... .
+
+	6 Contribu tor address : City; State; Zip Code
+
+	Houston, TX 7709 2
+	8
+
+	Principal occup.a6on / Job title (See Instructions)
+
+	9
+
+	CPA
+	Date
+
+	09/1012018
+
+	Employer (See Instructions)
+
+	Self
+	Full name o f con tributor
+
+	0
+
+	\
+
+	out-of-slate PAC (10 #:
+
+	Amount o f Contribution ($)
+
+	Law Office of Mitche ll R Ne lson PLLC
+
+	$2,500 .00
+
+	.. ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... .
+
+	Contribu tor address : City; State; Zip Code
+
+	Houston, TX 77008
+	Principal occup.a6on / Job title (See Instructions)
+
+	Date
+
+	09/2712018
+
+	Full name o f con tributor
+
+	0
+
+	Employer (See Instructions )
+
+	\
+
+	out-of-slate PAC (10 #:
+
+	Amount o f Contribution ($)
+
+	Law Offices of Matt hew Dexter PLLC
+
+	$2 50.00
+
+	.. ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... .
+
+	Contribu tor address : City; State; Zip Code
+
+	Houston, TX 7700 9
+	Principal occup.a6on / Job title (See Instructions)
+
+	Date
+
+	09/1712018
+
+	Full name o f con tributor
+
+	Employer (See Instructions )
+
+	D out-of-slate PAC (10 #:
+
+	\
+
+	Amount o f Contribution ($)
+
+	McAlister, Sea n
+
+	$2,500 .00
+
+	.. ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... .
+
+	Contribu tor address : City; State; Zip Code
+
+	Spring, TX 77379
+	Principal occup.a6on / Job title (See Instructions)
+
+	Employer (See Instructions )
+
+	Attorney
+
+	Self
+
+	Date
+
+	09/0 612018
+
+	Full name o f con tributor
+
+	D out-of-slate PAC (10 #:
+
+	\
+
+	McBride, Ralph
+
+	Amount o f Contribution ($)
+
+	$1,000 .00
+
+	.. ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... .
+
+	Contribu tor address : City; State; Zip Code
+
+	Houston, TX 77057
+	Principal occup.a6on / Job title (See Instructions)
+
+	Employer (See Instructions )
+
+	Attorney
+
+	Bracewe ll
+
+	Forms prov1aea D'y Texas Eli ,,cs .....
+	omm1ss1on
+
+	www.eti,1cs.state.tx.us
+
+	Versmn Vl. U.o.t:o3
+`,
+	"Abbyy": `
+	MONETARY POLITICAL CONTRIBUTIONS
+	                                                                                                                                                                                                                        SCHEDULE A1
+
+
+
+
+	                                                                                                                                                                                            1 Total pages Schedule Al:
+	              The Instruction Guide explains how to complete this form.
+	                                                                                                                                                                                            Sch: 2/3 Rpl: 5/7
+
+
+	 2 FILER NAME                                                                                                                                                                               3 Filer ID (Ethics Commission Filers)
+	              Citizens for a Quality Judiciary                                                                                                                                              00082952
+
+	                                              5 Full name of contributor Q out-ot-stato PAC (ID»                                                                                            7 Amount of Contribution ($)
+	 4 Date
+	              08/07/2018                      Larry M Hicks CPA                                                                                                                                                                                 S100.00
+
+
+	                                              6 Contributor address; City; State; Zip Code
+
+
+
+
+	                                                  Houston. TX 77092
+
+	              Principal occupation / Job title (See Instructions)                                                                           9 Employer (See Instructions)
+
+	              CPA                                                                                                                           Self
+
+	              Date                                              Full name of contributor [_J out-ot-stato PAC (ID»                                                            .!                               Amount of Contribution ($)
+	              09/10/2018                                        Law Office of Mitchell R Nelson PLLC                                                                                                                                          $2,500.00
+	                                                                Contributor address; City; Stato; Zip Code
+
+
+
+
+	                                                  Houston. TX 77008
+
+
+	                                              Principal occupation / Job title (See Instructions)                                             Employer (See Instructions)
+
+
+
+	              Date                                              Full name of contributor jj out-of-staie PAC <10"■                                                                                             Amount ot Contribution ($)
+	              09/27/2018                                        Law Offices of Matthew Dexter PLLC                                                                                                                                              $250 00
+
+	                                                  Contributor address; City; State; Zip Code
+
+
+
+	                                                  Houston, TX 77009
+
+	                                              Principal occupation / Job title (See instructions)                                             Employer (See Instructions)
+
+
+
+	              Date                                Full name of contributor                              □ cuto< slate PAC PD=                                                                                  Amount of Contribution (S)
+	              09/17/2018                          McAlister. Sean                                                                                                                                                                             $2.500 00
+
+	                                                  Contributor address; City. State; Zip Code
+
+
+
+	                                                  Spring. TX 77379
+	                                                                                                 «wj    I    imi
+	              Principal occupation / Job tide (See instructions)                                                                                                  Employer (See Instructions)
+	              Attorney                                                                           _|    Sell                                                       Self
+
+	                                                                                                 p out-oT-siate PAC PD*_
+	              Date                                Full name of contributor                                                                                                    J                                Amount of Contribution ($)
+
+	              09/06/2018                          McBride. Ralph                                                                                                                                                                              $1.000 00
+
+	                                                  Contributor address; City State; Zip Code
+
+
+
+	                                                  Houston. TX 77057
+
+	              Principal occupation / Job tide (See Instructions)                                                                              Employer (See Instructions)
+	              Attorney                                                                                                                        Bracewell
+
+
+
+
+
+	-or ms irovided by Texas Ethics Commission                                                                           www.ethics.state.tx.us                                                                                                     viAWftS
+`,
+  "Google": `
+	MONETARY POLITICAL CONTRIBUTIONS
+	SCHEDULE A1
+	1
+	Total pages Schedule A1:
+	Sch: 113 Rpt: 4/7
+	Filer ID
+	00082952
+	Amount of Contribution ($)
+	The Instruction Guide explains how to complete this form.
+	2 FILER NAME
+	3
+	(Ethics Commission Filers)
+	Citizens for a Quality Judiciary
+	4 Date
+	Full name of contributorout-of-state PAC (ID
+	out-of-state PAC (ID#:
+	7
+	09/17/2018 Bloome, Sara
+	$2,000.00
+	6 Contributor address; City State; Zip Code
+	Houston, TX 77098
+	8
+	Principal occupation / Job title (See Instructions)
+	Attorney
+	Date
+	09/27/2018 Bobbitt, JB
+	9 Employer (See Instructions)
+	Self
+	Full name of contributorout-of-state PAC (ID
+	Amount of Contribution ($)
+	$1,000.00
+	Contributor address; City: State; Zip Code
+	Houston, TX 77080
+	Principal occupation / Job title (See Instructions)
+	Attorney
+	Date
+	09/27/2018 Brock, Valeria Lee
+	Employer (See Instructions)
+	Self
+	Full name of contributorout-of-state PAC (ID
+	Amount of Contribution ($)
+	$2,500.00
+	Contributor address; City: State; Zip Code
+	Houston, TX 77098
+	Principal occupation / Job title (See Instructions)
+	Attorney
+	Date
+	09/10/2018 Cannon, Joseph
+	Employer (See Instructions)
+	Self
+	Full name of contributorout-of-state PAC (ID
+	Amount of Contribution ($)
+	$3,000.00
+	Contributor address; City: State; Zip Code
+	Spring, TX 77389
+	Principal occupation / Job title (See Instructions)
+	Attorney
+	Date
+	09/12/2018ttinger, Brian
+	Employer (See Instructions)
+	Self
+	Full name of contributorout-of-state PAC (ID
+	Amount of Contribution ($)
+	$2,500.00
+	Contributor address; City: State; Zip Code
+	Houston, TX 77094
+	Principal occupation / Job title (See Instructions)
+	Attorney
+	Employer (See Instructions)
+	Law Office of Brian Ettinger
+	orms provided by Texas Ethics Commission
+	www.ethics.state.tx.us
+	ersion
+`,
+	"Azure": `
+	MONETARY POLITICAL CONTRIBUTIONS
+	The Instruction Guide explains how to complete this form.
+	2 FILER NAME
+	Citizens for a Quality Judiciary
+	SCHEDULE Al
+	I Total pages Schedule Al:
+	sch: 2,'3 Rpt: 5/7
+	3 Filer ID (Ethics Commission Filers)
+	00082952
+	7 Amount of Contribution (S)
+	4 Date
+	08/07/2018
+	S Full name of contributor
+	cwt-of-State PAC (10#:
+	Larry M Hicks CPA
+	Contributor address; City: State; Zip Code
+	Houston, TX 77092
+	Principal occupation / Job title (See Instructions)
+	CPA
+	09/10/2018
+	Full name of contributor
+	cwt-of-State PAC (10#:
+	Law Office Of Mitchell R Nelson PLI_C
+	Contributor address; City: State; Zip Code
+	Houston, TX 7700B
+	Principal occupation / Job title (See Instructions)
+	09/27/2018
+	Full name of contributor
+	cwt-of-State PAC (10#:
+	Law Offices Of Matthew Dexter PLLC
+	Contributor address; City: State; Zip Code
+	Houston, TX 7700g
+	Principal occupation / Job title (See Instructions)
+	09/17/2018
+	Full name of contributor
+	cwt-of-State PAC (10#:
+	McAlister, Sean
+	Contributor address; City: State; Zip Code
+	spring, TX 7737g
+	Principal occupation / Job title (See Instructions)
+	Attorney
+	09/06/2018
+	Employer (See Instructions)
+	self
+	Employer (See Instructions)
+	Employer (See Instructions)
+	Employer (See Instructions)
+	self
+	Employer (See Instructions)
+	Bracewell
+	$100.00
+	Amount of Contribution (S)
+	$2,500.00
+	Amount of Contribution (S)
+	$250.00
+	Amount of Contribution (S)
+	$2,500.00
+	Amount of Contribution (S)
+	$1,000.00
+	Full name of contributor
+	cwt-of-State PAC (10#:
+	Mcdride, Ralph
+	Contributor address; City: State; Zip Code
+	Houston, TX 77057
+	Principal occupation / Job title (See Instructions)
+	Attorney
+	rms prov
+	exas
+	CS
+	omm ss on
+	WV.".N.
+	CSState.tX.us
+
+	`
+}
