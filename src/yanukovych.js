075f19696302cc4ba61f224999d2626a180cf212
Amanda on Rajje <amanda@factful.io>
2019-01-28 16:49:43 -0800

    adding framework for #6

---
+++ src/yanukovych.js
@@ -0,0 +1,12 @@
+export default {
+	"Original": "/images/{filename}",
+	"Transcript": ``,
+  "Attention OCR": ``,
+  "Calamari": ``,
+  "OCRopus": ``,
+	"Tesseract": ``,
+  "Adobe": ``,
+	"Abbyy": ``,
+  "Google": ``,
+	"Azure": ``
+}
